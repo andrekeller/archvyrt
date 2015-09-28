a7f96208c1538d199d7f061d046e82a78d1e64a8
Andre Keller <ak@0x2a.io>
2015-09-28 22:52:02 +0200

    Fix setup.py.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name='archvyrt',
-    version='0.1.0',
+    version='0.1.1',
     description='libvirt provisioner for archlinux libvirt hosts',
     url='https://github.com/andrekeller/archvyrt',
     author='Andre Keller',
@@ -19,13 +19,16 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: System Administrators',
-        'Topic :: System :: SystemAdministration',
+        'Topic :: System :: Systems Administration',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
     ],
 
     packages=find_packages(),
+    install_requires=[
+        'libvirt-python'
+    ],
 
     entry_points={
         'console_scripts': [
