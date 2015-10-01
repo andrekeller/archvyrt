7b7974746fb81740519de68d6256ca43d216f7bd
Andre Keller <ak@0x2a.io>
2015-10-02 00:25:40 +0200

    Fix another unicode occurrence.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -224,7 +224,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
   (master_doc, 'archvyrt.tex', 'archvyrt Documentation',
-   'André Keller', 'manual'),
+   'Andre Keller', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
