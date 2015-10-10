0f06a31cedd30d19b0a31a2124f1d04f6bb12645
Andre Keller <ak@0x2a.io>
2015-10-10 13:29:17 +0200

    Release 0.3.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name='archvyrt',
-    version='0.3.2',
+    version='0.3.3',
     description='libvirt provisioner for archlinux libvirt hosts',
     url='https://github.com/andrekeller/archvyrt',
     author='Andre Keller',
